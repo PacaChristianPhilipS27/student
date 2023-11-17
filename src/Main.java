4f2286ce22ce6119d30eb191e8ab3a775b7980ec
PacaChristianPhilipS27 <joshuaquidit123456@gmail.com>
2023-11-17 15:20:53 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+      public static void main(String[] args) {
+    
+         Studyante studyante = new Studyante("Christian Philip Paca" , "IT21C" , "be contented of what ypu have");
+        
+        System.out.println("Name: " + studyante.getName());
+        System.out.println("Section: " + studyante.getSection());
+        System.out.println("Motto: " + studyante.getMotto());
+    }
+}
+    
---
+++ src/Studyante.java
@@ -0,0 +1,24 @@
+
+class Studyante {
+    String name;
+    String section;
+    String motto;
+    
+    Studyante (String name ,String section, String motto) {
+        this.name = name;
+        this.section = section;
+        this.motto = motto;
+    }
+
+    String getName() {
+        return name;
+    }
+    String getSection() {
+        return section;
+    }
+    String getMotto() {
+        return motto;
+    }
+}
+    
+
